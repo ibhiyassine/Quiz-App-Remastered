52afdb098b9b6ec4727e1f56f55b152052bd3528
ibhiyassine <ibhiyassine@gmail.com>
2025-04-16 01:42:08 +0100

    Added Carousel, Leaderboard, Main Quiz Card

---
+++ src/components/TopicTag.vue
@@ -0,0 +1,26 @@
+<script setup>
+const props = defineProps({
+    topic: {
+        type: String,
+        required: true,
+        default: "topic",
+    }
+});
+</script>
+
+<template>
+    <div class="rounded-pill d-inline px-2 py-1 tag">
+        {{ topic }}
+    </div>
+</template>
+
+<style scoped>
+.tag{
+    color: var(--secondary-color);
+    color: rgb(107, 157, 172);
+    border: solid 1px var(--secondary-color);
+    background-color: rgba(29, 60, 69, 1);
+    font-size: 1rem;
+    text-align: center;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginRegisterView from '@/views/LoginRegisterView.vue'
+import HomeView from '@/views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      redirect: '/login'
+      name: 'home',
+      component: HomeView
     },
     {
       path: '/login',
